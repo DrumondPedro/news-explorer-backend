60daf083e1c00f3e349a3acd3c4177c0cdf6008b
pedrumond <pe.drumond97@gmail.com>
2025-05-13 15:37:18 -0300

    refactor: put connection URL in an environment variable

--- data/database.js
+++ data/database.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
+const { DATABASE_URL } = process.env;
+
 function connectDatabase() {
   mongoose
-    .connect("mongodb://localhost:27017/newsexplorerdb", {
+    .connect(DATABASE_URL, {
       serverSelectionTimeoutMS: 2000,
     })
     .then(() => console.log("MongoDB conectado com sucesso"))
